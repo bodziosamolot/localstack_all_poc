910228790827c43ac32bf8d9c3f8d9a2a4ac7ce0
Jarosław Ogiegło <jogieglo@future-processing.com>
2020-12-11 17:17:58 +0100

    in progress

--- init.bat
+++ init.bat
@@ -1,5 +1,9 @@
 git init
 meta init
+
 meta project import common git@github.com:bodziosamolot/localstack_common_poc.git
 meta project import consume git@github.com:bodziosamolot/localstack_consumer_poc.git
 meta project import producer git@github.com:bodziosamolot/localstack_producer_poc.git
+
+git remote add origin git@github.com:bodziosamolot/localstack_all_poc.git
+git push --set-upstream origin master
