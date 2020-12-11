7211addaa3ef4792ac9853f242f9960c1cd19ab6
Jarosław Ogiegło <jogieglo@future-processing.com>
2020-12-11 17:14:43 +0100

    in progress

---
+++ init.bat
@@ -0,0 +1,5 @@
+git init
+meta init
+meta project import common git@github.com:bodziosamolot/localstack_common_poc.git
+meta project import consume git@github.com:bodziosamolot/localstack_consumer_poc.git
+meta project import producer git@github.com:bodziosamolot/localstack_producer_poc.git
